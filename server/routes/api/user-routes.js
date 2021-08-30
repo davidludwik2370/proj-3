1a94e29e8ac1299223556e75e4e5bdc231dc60e6
Rahkeem <ancientmiths@gmail.com>
2021-08-30 10:43:53 -0400

    google

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -16,3 +16,20 @@ router.route('/login').post(login);
 router.route('/me').get(authMiddleware, getSingleUser);
 
 module.exports = router;
+
+function calcRoute() {
+  var selectedMode = document.getElementById('mode').value;
+  var request = {
+      origin: haight,
+      destination: oceanBeach,
+      // Note that JavaScript allows us to access the constant
+      // using square brackets and a string value as its
+      // "property."
+      travelMode: google.maps.TravelMode[selectedMode]
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == 'OK') {
+      directionsRenderer.setDirections(response);
+    }
+  });
+}
